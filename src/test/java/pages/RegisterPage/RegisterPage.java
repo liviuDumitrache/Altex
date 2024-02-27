fe37cb7811817c76c959df83dd8749e8489ec5ce
liviuDumitrache <liviudumitrache1@gmail.com>
2024-02-28 00:11:57 +0200

    Lucrat la formularul de  regiter

--- src/test/java/pages/RegisterPage/RegisterPage.java
+++ src/test/java/pages/RegisterPage/RegisterPage.java
@@ -16,7 +16,25 @@ public class RegisterPage extends BasePage {
         return instance;
     }
 
+    private By inregistrareButton = By.xpath("//a[@class='underline hover:text-bleu-hover text-red-brand text-sm leading-15.6']");
+    private By imputPrenume = By.xpath("//input[@label='Prenume']");
+    private By imputNume = By.xpath("//input[@label='Nume']");
 
+
+    public void clickInregistrareButton() {
+        LOG.info("Click 'Inregistrare' button");
+        driver.findElement(inregistrareButton).click();
+    }
+
+    public void insertPrenume(String prenume) {
+        LOG.info("Insert prenume");
+        driver.findElement(imputPrenume).sendKeys(prenume);
+    }
+
+    public void insertNume(String nume){
+        LOG.info("Insert prenume");
+        driver.findElement(imputNume).sendKeys(nume);
+    }
 
 
 
--- src/test/java/test/RegisterTest/RegisterTest.java
+++ src/test/java/test/RegisterTest/RegisterTest.java
@@ -5,16 +5,35 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import test.BaseTest;
 
+import static pages.BasePage.driver;
 import static pages.BasePage.getBaseUrl;
 
 public class RegisterTest extends BaseTest {
     public static final Logger LOG = LoggerFactory.getLogger(RegisterTest.class);
     private String newUrl = getBaseUrl() + "Register.html";
 
+    String prenume = "Ion";
+    String nume = "Popescu";
+    String email = "maroha2658@huizk.com";
+    String phone = "0745123456";
+    String parola = "Parola1";
+
     @Test
 
     public void register(){
         signInPage.clickContButton();
+
+        LOG.info("Click 'Inregistrare' button ");
+        registerPage.clickInregistrareButton();
+
+        LOG.info("Type in Prenume");
+        registerPage.insertPrenume(prenume);
+
+        LOG.info("Type in Nume");
+        registerPage.insertPrenume(nume);
+
+
+
 
     }
 
